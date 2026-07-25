00b641261fae4119c1b81ee4db8bedf30bda0f67
mikechu-2006 <mikechu-2006@github.com>
2026-07-25 16:04:43 +0800

    1

--- run_pipeline_gcg_roleplay_init.sh
+++ run_pipeline_gcg_roleplay_init.sh
@@ -7,4 +7,4 @@
 #SBATCH -t 7-00:00:00
 
 
-python main.py --pipeline configs/GCG-Llama-3.1-8B-Instruct-roleplay-init-cluster-2c.yaml
+python main.py --pipeline configs/GCG-Llama-3.1-8B-Instruct-roleplay-init-cluster-3c.yaml
